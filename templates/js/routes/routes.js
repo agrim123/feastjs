49135a1e22bba1c1e95b994f1f9d197f1e8659d2
hitman <agrimmittal97@gmail.com>
2017-05-09 00:59:16 +0530

    add controller generator

--- templates/js/routes/routes.js
+++ templates/js/routes/routes.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var index = require('../app/controllers/index_controller');
+var index = require('../app/controllers/index_controller')
 
-router.get('/', index.home);
+router.get('/', index.home)
 
-module.exports = router;
+module.exports = router
